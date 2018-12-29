7518b18b640a0cb7bbf8ac87a640ad8d137f965c
Beata <becia09269@wp.pl>
2018-12-29 01:32:10 +0100

    first version

---
+++ cs1/cs1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs1
+{
+    public class Hello
+    {
+        public static string FizzBuzz()
+        {
+            //int number = Console.ReadLine() ;
+            //Console.WriteLine("Podaj numer w zkresie 0 - 1000");
+            int number = 0;
+            bool flag;
+
+            do
+            {
+                Console.WriteLine("Podaj numer w zakresie 0 - 1000");
+                flag = Int32.TryParse(Console.ReadLine(), out number);
+                Console.WriteLine(number);
+                if (flag == false)
+                {
+                    Console.WriteLine("Wartość niepoprawna, spróbuj jeszcze raz");
+                }
+                else if (number < 0 || number > 1000)
+                {
+                    flag = false;
+                    Console.WriteLine("Podana liczba jest mniejsza od 0 lub większa od 1000, spróbuj jeszcze raz");
+                }
+            }
+            while (flag == false);
+
+            Console.WriteLine(number);
+            return "d";
+        }
+
+        public static void Main()
+        {
+            char choice;// = ' ';
+
+            for (;;)
+            {
+                Console.Clear();
+                Console.WriteLine("Co mogę dla ciebie zrobić ;) ?");
+                Console.WriteLine("1. FizzBuzz");
+                Console.WriteLine("2. DeepDive");
+                Console.WriteLine("3. DrownItDown");
+                Console.WriteLine("4. Exit");
+
+                //choice = Console.ReadLine();    //wybor
+                choice = Console.ReadKey(true).KeyChar;
+
+                Console.WriteLine(choice);
+
+                switch (choice)
+                {
+                    case '1':
+                        Console.Clear();
+                        Console.WriteLine("Wybierasz 1. FizzBuzz");
+                        FizzBuzz();
+                        break;
+
+                    case '2':
+                        Console.Clear();
+                        Console.WriteLine("Wybierasz 2. DeepDive");
+                        break;
+
+                    case '3':
+                        Console.Clear();
+                        Console.WriteLine("Wybierasz 3. DrownItDown");
+                        break;
+
+                    case '4':
+                        Console.Clear();
+                        Console.WriteLine("Wybierasz 4. Exit");
+                        System.Environment.Exit(1);
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("To niepoprawna wartość, nie wiem co mam zrobić ?");
+                        break;
+                }
+                Console.ReadKey();
+                //Console.Clear();
+            }
+        }
+    }
+}
