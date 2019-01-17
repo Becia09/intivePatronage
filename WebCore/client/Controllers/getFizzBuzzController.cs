bf39c612eb2a9eefef6e26b8c51e24fb1caca1ce
Beata <becia09269@wp.pl>
2019-01-17 01:30:05 +0100

    add getFizzBuzz

--- WebCore/client/Controllers/getFizzBuzzController.cs
+++ WebCore/client/Controllers/getFizzBuzzController.cs
@@ -3,47 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Text;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace client.Controllers
 {
-    [Route("api/[controller]")]
-    public class getFizzBuzzController : Controller
+    [Route("[controller]")]
+    public class GetFizzBuzzController : Controller
     {
-        private static readonly HttpClient client = new HttpClient();
-
-        // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET api/<controller>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
-        }
+            string html = string.Empty;                             //wyrzucić do odzielnej klasy; ustawić domyślny kontroler
+            string url = "https://localhost:44394/fizzbuzz/" + id;
 
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
 
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            //request.Method = "GET";
+            //request.ContentType = "application/x-www-form-urlencoded";
+            //request.AutomaticDecompression = DecompressionMethods.GZip;
 
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader readStream = new StreamReader(stream, Encoding.UTF8);
+            //StreamReader reader = new StreamReader(stream);
+            html = readStream.ReadToEnd();
+
+            response.Close();
+            readStream.Close();
+
+            return html;
         }
     }
 }
