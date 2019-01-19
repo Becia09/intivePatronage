b1e01368253294924e812d1effb91816cba6b454
Beata <becia09269@wp.pl>
2019-01-19 14:19:14 +0100

    update rest api

--- WebCore/WebCore/Controllers/RoomController.cs
+++ WebCore/WebCore/Controllers/RoomController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebCoreApi.Controllers
@@ -16,49 +17,24 @@ namespace WebCoreApi.Controllers
         private static List<Room> roomList = new List<Room>();
         //private int roomId = 0;
 
-        // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
 
         // GET api/<controller>/add
         //https://localhost:44394/room/add/pok1
-        [HttpGet("add/{name}")]
-        public int GetAdd(string name)
+        [HttpPut("add")]
+        public int PutAdd(Models.RoomModel rm)
         {
             //bool flag = false;
-            Room r = new Room(name);
+            Room r = new Room(rm.name);
             roomList.Add(r);
             return r.id;
-        }
-
-        // GET api/<controller>/add
-        //https://localhost:44394/room/add2/pok1
-        [HttpGet("add2/{name}")]
-        public int GetAdd2(string name)
-        {
-            //bool flag = false;
-            Room r = new Room(name);
-            roomList.Add(r);
-            //r = new Room(name);
-            //roomList.Add(r);
-            return roomList.Count;
+            
         }
 
         // GET api/<controller>/del
         //https://localhost:44394/room/del/2
-        [HttpGet("del/{id}")]
-        public int GetDelete(int id)
+        [HttpDelete("delete/{id}")]
+        public int Delete(int id)
         {
             //var item = roomList.Single(Room => Room.id == 2);
             //roomList.Remove(item);
@@ -70,13 +46,13 @@ namespace WebCoreApi.Controllers
 
         // GET api/<controller>/add
         //https://localhost:44394/room/edit/2&name2
-        [HttpGet("edit/{id}&{newName}")]
-        public string GetEdit(int id, string newName)
+        [HttpPatch("edit/{id}")]
+        public string GetEdit(int id, Models.RoomModel rm)
         {
             if (roomList.Exists(Room => Room.id == id))
             {
                 Room rEdit = roomList.Find(Room => Room.id == id); //zwraca pierwszy element spełniający warunek, jeśli nie ma elementu to wartość domyślną dla typu elementó listy
-                rEdit.name = newName;
+                rEdit.name = rm.name;
                 return rEdit.name;
             }
             else
@@ -117,27 +93,8 @@ namespace WebCoreApi.Controllers
 
             return list;
         }
+        
 
-
-
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
---
+++ WebCore/WebCore/Models/RoomModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreApi.Models
+{
+    public class RoomModel
+    {
+        public string name { get; set; }
+    }
+}
